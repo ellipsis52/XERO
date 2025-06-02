1db342c7ad17f9b409023221b11a39b7b338f90f
Nagle Steve <nagle@webtechnicom.net>
2025-06-02 21:10:57 +0200

    Intégration initiale React + Vite avec structure front-end réorganisée

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -5,4 +6,12 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+=======
+import React from 'react';
+import { render } from "@testing-library/react";
+import App from './App';
+
+it('renders without crashing', () => {
+  render(<App />);
+>>>>>>> 34d1dd5 (Added React integration to Hyperspace)
 });
